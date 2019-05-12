8fa95a6cee4692c99326113312de258cb43343c3
Vivek Hari <vivek.hari@outlook.co.nz>
2019-05-12 20:18:28 +1200

    Use mode-specific ratings when determing good perk configurations.

--- src/services/ArmourComparer.ts
+++ src/services/ArmourComparer.ts
@@ -7,6 +7,21 @@ interface PerkTreeNode {
     perk: DestinyPerkContainer;
     children: PerkTreeNode[];
 }
+enum PerkComparisonResult {
+    PERK_IS_INCOMPARABLE = 0,
+    PERK_IS_EQUIVALENT = 1,
+    PERK_IS_BETTER = 2
+}
+enum PerkConfigComparisonResult {
+    CONFIG_IS_INCOMPARABLE = 0,
+    CONFIG_IS_EQUIVALENT = 1,
+    CONFIG_IS_BETTER = 2
+}
+enum ConfigInclusion {
+    CONFIG_NOT_INCLUDED = 0,
+    CONFIG_IS_INCLUDED = 1,
+    BETTER_CONFIG_INCLUDED = 2
+}
 
 export default class ArmourComparer {
     perkRatings: Map<string, PerkRating>
@@ -16,6 +31,9 @@ export default class ArmourComparer {
     }
 
     compare(item1: DestinyItemContainer, item2: DestinyItemContainer): ItemComparisonResult {
+        if (item1.item.id === '6917529085856201652' && item2.item.id === '6917529092262278684') {
+            debugger;
+        }
         if (item1.definition.class !== item2.definition.class) {
             return ItemComparisonResult.ITEM_IS_INCOMPARABLE;
         }
@@ -33,105 +51,122 @@ export default class ArmourComparer {
         }
 
         // determine good perks on each item
-        let item1GoodPerks = this.getGoodPerks(item1);
-        let item2GoodPerks = this.getGoodPerks(item2);
+        let item1PerkConfigs = this.getGoodPerkConfigs(item1);
+        let item2PerkConfigs = this.getGoodPerkConfigs(item2);
 
-        return this.comparePerks(item1GoodPerks, item2GoodPerks);
+        return this.compareItemPerkConfigs(item1PerkConfigs, item2PerkConfigs);
     }
 
-    private getGoodPerks(container: DestinyItemContainer): DestinyPerkContainer[][] {
-        return container.perkColumns.map(
-            column => column.filter(perk => perk && (perk.isGoodByMode['PvE'] || perk.isGoodByMode['PvP'])));
-    }
+    private compareItemPerkConfigs(item1PerkConfigs: DestinyPerkContainer[][], item2PerkConfigs: DestinyPerkContainer[][]): ItemComparisonResult {
+        // test whether item 1 contain's all of item 2's configs
+        let i1ConfigInclusion = item2PerkConfigs.map(i2Config => this.determineConfigInclusion(i2Config, item1PerkConfigs));
 
-    private comparePerks(item1Perks: DestinyPerkContainer[][], item2Perks: DestinyPerkContainer[][]): ItemComparisonResult {
-        let item1PerkConfigs = this.getPerkConfigs(item1Perks);
-        let item2PerkConfigs = this.getPerkConfigs(item2Perks);
+        // test whether item 2 contain's all of item 1's configs
+        let i2ConfigInclusion = item1PerkConfigs.map(i1Config => this.determineConfigInclusion(i1Config, item2PerkConfigs));
 
-        let i1ConfigInclusion = this.scoreConfigInclusion(item1PerkConfigs, item2PerkConfigs);
-        if (i1ConfigInclusion === 2) {
-            // item 2 contains all of item 1's configurations and at least one improved version
-            return ItemComparisonResult.ITEM_IS_BETTER;
-        } else if (i1ConfigInclusion === 1) {
-            if (item2PerkConfigs.length > item1PerkConfigs.length) {
-                // item 2 contains all of item 1's configurations, but also has additional good configurations
-                return ItemComparisonResult.ITEM_IS_BETTER;
+        if (i2ConfigInclusion.some(comparison => comparison === ConfigInclusion.CONFIG_NOT_INCLUDED)) {
+            // item 2 does not contain all of item 1's configs
+            if (i1ConfigInclusion.some(comparison => comparison === ConfigInclusion.CONFIG_NOT_INCLUDED)) {
+                // item 1 does not contain all of item 2's configs
+                return ItemComparisonResult.ITEM_IS_INCOMPARABLE;
             } else {
-                // item 2 contains all of item 1's configurations, but no others
+                // item 1 contains all of item 2's configs
+                return ItemComparisonResult.ITEM_IS_WORSE;
+            }
+        } else if (i2ConfigInclusion.some(comparison => comparison === ConfigInclusion.BETTER_CONFIG_INCLUDED)) {
+            // item 2 contains a better version of at least one of item 1's configs
+            if (i1ConfigInclusion.some(comparison => comparison === ConfigInclusion.CONFIG_NOT_INCLUDED)) {
+                // item 1 does not contain all of item 2's configs
+                return ItemComparisonResult.ITEM_IS_BETTER;
+            } else if (i1ConfigInclusion.some(comparison => comparison === ConfigInclusion.BETTER_CONFIG_INCLUDED)) {
+                // item 1 contains a better version of at least one of item 2's configs
+                // todo: test which has a higher number of 'better' configs
                 return ItemComparisonResult.ITEM_IS_EQUIVALENT;
+            } else {
+                // item 1 contains all of item 2's configs
+                return ItemComparisonResult.ITEM_IS_BETTER;
             }
         } else {
-            let i2ConfigInclusion = this.scoreConfigInclusion(item2PerkConfigs, item1PerkConfigs);
-            if (i2ConfigInclusion >= item2PerkConfigs.length) {
-                // item 1 contains all of item 2's configurations
+            // item 2 contains all of item 1's configs
+            if (i1ConfigInclusion.some(comparison => comparison === ConfigInclusion.CONFIG_NOT_INCLUDED)) {
+                // item 1 does not contain all of item 2's configs
+                return ItemComparisonResult.ITEM_IS_BETTER;
+            } else if (i1ConfigInclusion.some(comparison => comparison === ConfigInclusion.BETTER_CONFIG_INCLUDED)) {
+                // item 1 contains a better version of at least one of item 2's configs
                 return ItemComparisonResult.ITEM_IS_WORSE;
             } else {
-                // neither item includes all of the other's configurations
-                return ItemComparisonResult.ITEM_IS_INCOMPARABLE;
+                // item 1 contains all of item 2's configs
+                return ItemComparisonResult.ITEM_IS_EQUIVALENT;
             }
         }
     }
 
-    private scoreConfigInclusion(configs1: DestinyPerkContainer[][], configs2: DestinyPerkContainer[][]): number {
-        if (configs1.length === 0) {
-            return 1;
+    private determineConfigInclusion(configToFind: DestinyPerkContainer[], configs: DestinyPerkContainer[][]): ConfigInclusion {
+        let comparisons = configs.map(configToTest => this.comparePerkConfigs(configToFind, configToTest));
+        if (comparisons.some(c => c === PerkConfigComparisonResult.CONFIG_IS_BETTER)) {
+            // item 2 contains a better version of this configuration
+            return ConfigInclusion.BETTER_CONFIG_INCLUDED;
+        } else if (comparisons.some(c => c === PerkConfigComparisonResult.CONFIG_IS_EQUIVALENT)) {
+            // item 2 contains this configuration
+            return ConfigInclusion.CONFIG_IS_INCLUDED;
         }
 
-        let bestScore = 0;
-        for (let i = 0; i < configs1.length; i++) {
-            const config = configs1[i];
-            let score = this.determineConfigInclusion(configs2, config);
-            if (score === 0) {
-                return 0;
+        // item 2 does not contain this configuration
+        return ConfigInclusion.CONFIG_NOT_INCLUDED;
+    }
+
+    private comparePerkConfigs(config1: DestinyPerkContainer[], config2: DestinyPerkContainer[]): PerkConfigComparisonResult {
+        let isMissingPerk: boolean = false;
+        let hasBetterPerk: boolean = false;
+
+        // test whether config 2 is better or equal to config 1
+        config1.forEach(perkToFind => {
+            let comparisons = config2.map(perkToTest => this.comparePerk(perkToFind, perkToTest));
+            if (comparisons.some(c => c === PerkComparisonResult.PERK_IS_BETTER)) {
+                // config 2 contains a better version of the perk
+                hasBetterPerk = true;
+            } else if (comparisons.some(c => c === PerkComparisonResult.PERK_IS_EQUIVALENT)) {
+                // config 2 contains the perk
             } else {
-                bestScore = Math.max(bestScore, score);
+                // config 2 does not contain the perk
+                isMissingPerk = true;
+                return;
             }
-        }
-        return bestScore;
-    }
-
-    private determineConfigInclusion(configs: DestinyPerkContainer[][], configToFind: DestinyPerkContainer[]): number {
-        // 0 = config not included
-        // 1 = exact config included
-        // 2 = better version of config included
-
-        // identify comparable perks for each column
-        let columns = configToFind.map(perk => {
-            let upgradeNames = this.getPerkUpgrades(perk).map(perk => perk.name);
-            return {
-                basePerk: perk.name,
-                upgradedPerks: upgradeNames
-            };
         });
 
-        let bestInclusionScore = 0;
-
-        for (let i = 0; i < configs.length; i++) {
-            const currentConfigNames = configs[i].map(perk => perk.name);
-
-            let inclusionScore = 0;
-            for (let c = 0; c < columns.length; c++) {
-                const comparablePerks = columns[c];
-                if (currentConfigNames.some(x => comparablePerks.upgradedPerks.some(y => y === x))) {
-                    // this config includes an upgraded version of the perk
-                    inclusionScore = Math.max(inclusionScore, 2);
-                } else if (currentConfigNames.some(perk => perk === comparablePerks.basePerk)) {
-                    // this config includes the perk
-                    inclusionScore = Math.max(inclusionScore, 1);
-                } else {
-                    // this config does not include a comparable perk
-                    inclusionScore = 0;
-                    break;
-                }
-            }
-            if (inclusionScore === 1 && currentConfigNames.length > columns.length) {
-                // this config includes all of the perks in the desired config (but no upgraded perks)
-                // it also includes additional perks
-                inclusionScore = 2;
-            }
-            bestInclusionScore = Math.max(bestInclusionScore, inclusionScore);
+        if (isMissingPerk) {
+            // config 2 does not contain one of the perks in config 1
+            return PerkConfigComparisonResult.CONFIG_IS_INCOMPARABLE;
         }
-        return bestInclusionScore;
+
+        // config 2 has all perks (or better versions of the perks) in config 1
+
+        if (hasBetterPerk) {
+            // config 2 has a better version of a perk in config 1
+            return PerkConfigComparisonResult.CONFIG_IS_BETTER;
+        }
+
+        if (config2.length > config1.length) {
+            // config 2 has more perks than config 1
+            return PerkConfigComparisonResult.CONFIG_IS_BETTER;
+        }
+
+        return PerkConfigComparisonResult.CONFIG_IS_EQUIVALENT;
+    }
+
+    private comparePerk(perk1: DestinyPerkContainer, perk2: DestinyPerkContainer): PerkComparisonResult {
+        if (perk2.name === perk1.name) {
+            // same perk
+            return PerkComparisonResult.PERK_IS_EQUIVALENT;
+        }
+
+        let upgrades = this.getPerkUpgrades(perk1);
+        if (upgrades.some(u => u.name === perk2.name)) {
+            // perk 2 is better than perk 1
+            return PerkComparisonResult.PERK_IS_BETTER;
+        }
+
+        return PerkComparisonResult.PERK_IS_INCOMPARABLE;
     }
 
     private getPerkUpgrades(perk: DestinyPerkContainer): DestinyPerkContainer[] {
@@ -152,9 +187,33 @@ export default class ArmourComparer {
         return upgrades;
     }
 
-    private getPerkConfigs(perkColumns: DestinyPerkContainer[][]): DestinyPerkContainer[][] {
-        let filteredColumns = perkColumns.filter(column => column.length > 0);
-        let perkTree = this.convertToTree(null, filteredColumns);
+    private getGoodPerkConfigs(item: DestinyItemContainer): DestinyPerkContainer[][] {
+        const modes = ['PvE', 'PvP'];
+
+        // identify good perks for each mode
+        let goodPerksForMode: DestinyPerkContainer[][][] =
+            modes.map(mode => item.perkColumns.map(column =>
+                column.filter(perk => perk && perk.isGoodByMode[mode])).filter(column => column.length > 0));
+
+        // identify available good perk configurations for each mode
+        let perkConfigsForMode = goodPerksForMode.map(perks => this.getPerkConfigs(perks));
+
+        // remove duplicate configs
+        let perkConfigs: DestinyPerkContainer[][] = [];
+        let existingConfigIds: string[] = [];
+        [].concat(...perkConfigsForMode).forEach((config: DestinyPerkContainer[]) => {
+            let id = config.map(perk => perk.name).join('+');
+            if (!existingConfigIds.includes(id)) {
+                existingConfigIds.push(id);
+                perkConfigs.push(config);
+            }
+        });
+
+        return perkConfigs;
+    }
+
+    private getPerkConfigs(perks: DestinyPerkContainer[][]): DestinyPerkContainer[][] {
+        let perkTree = this.convertToTree(null, perks);
         return this.getPerkConfigsFromTree(perkTree);
     }
 
